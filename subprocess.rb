ff335bd46a71adf88c578ec6ffc41293191709b9
Nick Booker <NMBooker@gmail.com>
2010-09-13 17:15:06 +0100

    Improved API docs

--- subprocess.rb
+++ subprocess.rb
@@ -14,12 +14,14 @@
 class Subprocess
   # the process id of the child
   attr_reader :pid
-  # set to the status of the child when wait is called
+  # set to the Process::Status of the child when wait is called
   attr_reader :status
   # the argument list originally passed
   attr_reader :args
 
-  # args: The argument list, including the program path at position 0
+  # Arguments:
+  # args:: The argument list to run, including the program path at position 0.
+  #        A list of strings.
   def initialize(args)
     @status = nil
     @args = args
@@ -29,7 +31,8 @@ class Subprocess
   end
 
   # Wait for the child process to exit.
-  # Returns and sets @status to a Status
+  # * Sets status to a Process::Status object.
+  # * Returns the same Process::Status object.
   def wait
     pid, status = Process.wait2(@pid)
     @status = status
@@ -41,5 +44,6 @@ if $PROGRAM_NAME == __FILE__
   puts "Testing ls..."
   child = Subprocess.new(['ls', '-l'])
   status = child.wait
+  puts "ls had pid: #{child.pid}"
   puts "ls exited with status #{status.exitstatus}"
 end
